7fd3209cdf9190ead5807c559dc55e039a01b603
mrrm <manuel.rodriguez@pucp.pe>
2013-10-16 14:10:51 +0200

    Creacion de clase test Persona: public String nombreCompletoMayus()

--- test/spai/egit2/test/PersonaTest.java
+++ test/spai/egit2/test/PersonaTest.java
@@ -8,43 +8,48 @@ import org.junit.Test;
 import spai.egit2.Persona;
 
 public class PersonaTest {
-    private Persona persona;
+	private Persona persona;
 
-    @Before
-    public void initObjects() {
-        this.persona = new Persona("Ana ", "Vega");
-    }
+	@Before
+	public void initObjects() {
+		this.persona = new Persona("Ana ", "Vega");
+	}
 
-    @Test
-    public void testGetNombre() {
-        assertEquals("Ana", this.persona.getNombre());
-    }
+	@Test
+	public void testGetNombre() {
+		assertEquals("Ana", this.persona.getNombre());
+	}
 
-    @Test
-    public void testSetNombre() {
-        this.persona.setNombre(" ana ");
-        assertEquals("Ana", this.persona.getNombre());
-    }
+	@Test
+	public void testSetNombre() {
+		this.persona.setNombre(" ana ");
+		assertEquals("Ana", this.persona.getNombre());
+	}
 
-    @Test
-    public void testGetApellido() {
-        assertEquals("Vega", this.persona.getApellido());
-    }
+	@Test
+	public void testGetApellido() {
+		assertEquals("Vega", this.persona.getApellido());
+	}
 
-    @Test
-    public void testSetApellido() {
-        this.persona.setApellido("  vega  ");
-        assertEquals("Vega", this.persona.getApellido());
-    }
+	@Test
+	public void testSetApellido() {
+		this.persona.setApellido("  vega  ");
+		assertEquals("Vega", this.persona.getApellido());
+	}
 
-    @Test
-    public void testInicial() {
-        assertEquals("A.", this.persona.inicial());
-    }
+	@Test
+	public void testInicial() {
+		assertEquals("A.", this.persona.inicial());
+	}
 
-    @Test
-    public void testNombreCompleto() {
-        assertEquals("Ana Vega", this.persona.nombreCompleto());
-    }
+	@Test
+	public void testNombreCompleto() {
+		assertEquals("Ana Vega", this.persona.nombreCompleto());
+	}
+
+	@Test
+	public void nombreCompletoMayus() {
+		assertEquals("ANA VEGA", this.persona.nombreCompletoMayus());
+	}
 
 }
